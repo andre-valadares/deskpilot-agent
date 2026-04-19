d532c8f69888d8a496dc719f76457ff9ddbc605e
André Valadares <andrelfv@live.com>
2026-04-18 23:02:15 -0300

    Add debug mode: SO_BROADCAST logs for debug

--- listen_windows.go
+++ listen_windows.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"context"
+	"log"
 	"net"
 	"syscall"
 
@@ -13,10 +14,25 @@ import (
 func listenWoL() (net.PacketConn, error) {
 	lc := net.ListenConfig{
 		Control: func(network, address string, c syscall.RawConn) error {
-			return c.Control(func(fd uintptr) {
-				_ = windows.SetsockoptInt(windows.Handle(fd), windows.SOL_SOCKET, windows.SO_BROADCAST, 1)
+			var setsockoptErr error
+			err := c.Control(func(fd uintptr) {
+				log.Printf("listenWoL: Control callback fd=%d", fd)
+				setsockoptErr = windows.SetsockoptInt(windows.Handle(fd), windows.SOL_SOCKET, windows.SO_BROADCAST, 1)
+				if setsockoptErr != nil {
+					log.Printf("listenWoL: SO_BROADCAST falhou: %v", setsockoptErr)
+				} else {
+					log.Println("listenWoL: SO_BROADCAST OK")
+				}
 			})
+			if err != nil {
+				return err
+			}
+			return setsockoptErr
 		},
 	}
-	return lc.ListenPacket(context.Background(), "udp4", ":9")
+	conn, err := lc.ListenPacket(context.Background(), "udp4", ":9")
+	if err == nil {
+		log.Println("listenWoL: socket UDP:9 aberto com sucesso")
+	}
+	return conn, err
 }
